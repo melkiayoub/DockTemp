6c9f72ad2c499b0946636dc6f4e45cbfa115da01
M.ayoub <ayoubelmelki19@gmail.com>
2025-04-16 01:26:34 +0100

    initial commit

---
+++ automation/index.js
@@ -0,0 +1,22 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { readFileSync } from "fs";
+
+yargs(hideBin(process.argv))
+  .command(
+    "auto <path>",
+    "Read and print file contents from given path",
+    (yargs) => {
+      return yargs.positional("path", {
+        type: "string",
+        describe: "Path to the config file",
+      });
+    },
+    (args) => {
+      const config = readFileSync(args.path, "utf-8");
+      console.log(config);
+    }
+  )
+  .demandCommand(1)
+  .strict() // Optional: disallow unknown commands
+  .parse();
